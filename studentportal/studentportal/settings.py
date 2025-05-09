e30955faa4547a0020e9db889a9545914bff64ae
esyjeteg17 <124711973+esyjeteg17@users.noreply.github.com>
2025-05-09 23:19:38 +0300

    front

--- studentportal/studentportal/settings.py
+++ studentportal/studentportal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
+import os
 
 from pathlib import Path
 
@@ -29,6 +30,12 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL= '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
 
 # Application definition

05ac9284e8c5a34d68c799731a1e69a67043a4a2
esyjeteg17 <124711973+esyjeteg17@users.noreply.github.com>
2025-05-10 08:40:59 +0300

    front

--- studentportal/studentportal/settings.py
+++ studentportal/studentportal/settings.py
@@ -17,6 +17,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL= '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -170,8 +176,8 @@ GIGACHAT_VERIFY_SSL = False  # или False, если нужно отключи�
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+""" STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static' """
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
